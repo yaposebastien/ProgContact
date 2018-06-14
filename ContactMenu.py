6b3fdd48ada67b09893651c189cb4b8e261c3635
Nke Sebastien Yapo <yaposebastien@gmail.com>
2018-06-14 07:42:38 -0400

    Add files via upload

--- ContactMenu.py
+++ ContactMenu.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3.6
+
 '''
 Ce programme est une amelioration d'un carnet d'adresse avec un fichier de type json
 
@@ -6,82 +8,78 @@ Ce programme est une amelioration d'un carnet d'adresse avec un fichier de type 
 
 import json
 
+#Creation liste de contacts pour les besoins de tests
+mesContacts = []
+mesContacts = ({ 'Momo': '+225454545', 'Marie-Paul': '+225787878'})
+
+
 #Menu de l'application
 def menuApplication():
-    print(f'\n\t Liste de contacts.')
-    print(f'\t\t Presser[1] pour Afficher toute la liste.')
-    print(f'\t\t Presser[2] pour Ajouter un nouveau contact.')
-    print(f'\t\t Presser[3] pour Rechercher un contact dans la liste.')
-    print(f'\t\t Presser[4] pour Supprimer un contact dans la liste.')
+    print('\f')
+    print(f'\n\t\t Application Carnet d"Addresses .')
+    print(f'\t\t Presser [L]ister tous vos contacts.')
+    print(f'\t\t Presser [A]jouter un nouveau contact.')
+    print(f'\t\t Presser [R]echercher un contact dans la liste.')
+    print(f'\t\t Presser [S]upprimer un contact dans la liste.')
+    print(f'\t\t Presser [Q]uitter votre carnet addresses .')
 
 #Ajouter un nouveau contact
 def AjouterContact():
-    with open('Contact_Telephonique', 'a') as monfichier:
-        nvellePersonne = input('Saisir le nom de la nouvelle personne')
-        nveauContact = input('Saisir le numero de telephone')
-        Personne = {contact: {'nom': nvellePersonne, 'telephone': nveauContact}}
-        monfichier.write(json.dumps(Personne))
-        monfichier.close()
+    with open('mesContacts.txt', 'a') as monfichier:
+        nvellePersonne = input('\t\t Saisir nom du nouveau contact: ')
+        nveauContact = input('\t\t Saisir le numero du nouveau contact: ')
+        mesContacts.update({ nvellePersonne : nveauContact }) #Important mot permettant l'ajout dans un dictionnaire de donnees
+        SauvegarderCarnet()
 
 
 
 #Rechercher un contact dans ma liste
 def RechercherContact():
-    with open('Contact_Telephonique') as MaListe:
-        mesContacts = json.load(MaListe)
-        
-        nomRecher = input('Saisir le nom a rechercher:')
-        
-        for personne in mesContacts['contact']:
-            if personne['nom'] == nomRecher:
-                print('Resultat de la recherche')
-                print('Nom: ' +personne['nom'])
-                print('Telephone: ' +personne['telephone'])
-                print('\n')
-                break
-            else:
-                print(f'{nomRecher} non retrouve!')
+        nomRecher = input('\t\t Saisir le nom a rechercher: ')
+
+        for Personne, Contact in mesContacts.items():
+
+            if Personne == nomRecher:
+                print('\n\t\t Resultat de votre recherche ')
+                print('\t\t', Personne, Contact, '\n')
+
 
 #Affichage de toute la liste des contacts
 def AfficherMaListe():
-    with open('Contact_Telephonique') as MaListe:
-        mesContacts = json.load(MaListe)
-        for personne in mesContacts['contact']:
-            print('Nom: ' +personne['nom'])
-            print('Telephone: ' +personne['telephone'])
-            print('\n')
+        for Personne, Contact in mesContacts.items():
+            print('\t\t', Personne, '---', Contact)
 
 #Supprimer un contact de la liste
-
 def SupprimerContact():
-    nomSupp = input('Saisir le nom a supprimer:')
-    with open('Contact_Telephonique') as MaListe:
-        mesContacts = json.load(MaListe)
-        for personne in mesContacts['contact']:
-            if personne['nom'] == nomSupp:
-                del personne['nom']
-                del personne['telephone']
-                ListeModifiee = json.dumps(mesContacts)
-                print(ListeModifiee)
+    nomSupp = input('\t\t Saisir le nom a supprimer: ')
+    del mesContacts[nomSupp] #Importante syntaxe permettant de supprimer un objet dans un dictionnaire
+    SauvegarderCarnet()
 
-                with open('Contact_Telephonique', 'w') as fichierModifier:
-                    json.dump(ListeModifiee, fichierModifier)
+#Sauvegarder votre carnet dans le fichier apres chaque modification
+def SauvegarderCarnet():
+    with open('mesContacts.txt', 'w') as monfichier:
+        json.dump(mesContacts, monfichier) #Importante syntaxe permettant d'ajouter notre dictionnaire au format json 
+        monfichier.close()
+
 
 
 
 if __name__ == '__main__' :
     
+
     continuer = False
     while continuer == False:
         menuApplication()
-        optionUtilisateur = input('\t Veuillez selectionner une action')
-        
-        if optionUtilisateur == '1':
-            AfficherMaListe()
-        elif optionUtilisateur == '2':
-            AjouterContact()
-        elif optionUtilisateur == '3':
-            RechercherContact()
-        elif optionUtilisateur == '4':
-            SupprimerContact()
+        optionUtilisateur = input('\n\t\t Veuillez selectionner une action: ')
 
+        if optionUtilisateur  in ["L", "l"]:
+            AfficherMaListe()
+        elif optionUtilisateur in ["A", "a"]:
+            AjouterContact()
+        elif optionUtilisateur in ["R", "r"]:
+            RechercherContact()
+        elif optionUtilisateur in ["S", "s"]:
+            SupprimerContact()
+        elif optionUtilisateur in ["Q", "q"]:
+            print("\t\t Fin de l'application! ")
+            continuer = True
